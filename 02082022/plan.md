00316b57ab97879790114e94123ef5d3e05319c0
JizongL <2015rpro@gmail.com>
2022-02-08 11:38:32 -0500

    updated dynamic programming content

---
+++ 02082022/plan.md
@@ -0,0 +1,7 @@
+Explain basic idea of dynmaic programming
+
+work through a warm up problem. 
+
+assign problems to the group working in groups, then regroup and work on the problem
+together. 
+
---
+++ 02082022/warmUp.js
@@ -0,0 +1,11 @@
+// Max non adjcent subset sum problem
+// write a function noAdjcentMaxSum that takes in an array of positive integers 
+// and return the maximum sum of non adjacent elements in the array. Return 0 if
+// the given array is empty. 
+
+
+function noAdjcentMaxSum(array){}
+
+const array = [75, 105, 120, 75, 90, 135];
+noAdjcentMaxSum(array); //should return 330
+
