6d61c0c2b73c66e38d6725189761d13a5560ae6f
JizongL <2015rpro@gmail.com>
2022-02-10 14:16:10 -0500

    added warmup for 02102022

---
+++ 02102022/warmUp.js
@@ -0,0 +1,7 @@
+// counting change
+// Write a function, changeCount, that takes an array that represents
+//  a stack of coins and a target amount of change. 
+// the function should return the number of different ways it is possible
+// to make change for the target amount using the coins. 
+
+countingChange(4, [1, 2, 3]); // 4
